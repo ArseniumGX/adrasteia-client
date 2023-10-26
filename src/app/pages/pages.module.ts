942dbd125a2103550c5dd7c2de608f4de21e46f5
José Pereira Macedo <arseniumgx@gmail.com>
2023-10-26 16:54:34 -0300

    feat: tela do solicitante

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,10 +4,17 @@ import { HomeComponent } from './home/home.component';
 import { SolicitacaoComponent } from './solicitacao/solicitacao.component';
 import { AdministracaoComponent } from './administracao/administracao.component';
 import { RouterModule } from '@angular/router';
+import { FormularioComponent } from './solicitacao/formulario/formulario.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [HomeComponent, SolicitacaoComponent, AdministracaoComponent],
-  imports: [CommonModule, RouterModule],
+  declarations: [
+    HomeComponent,
+    SolicitacaoComponent,
+    AdministracaoComponent,
+    FormularioComponent,
+  ],
+  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule],
   exports: [HomeComponent, SolicitacaoComponent, AdministracaoComponent],
 })
 export class PagesModule {}
